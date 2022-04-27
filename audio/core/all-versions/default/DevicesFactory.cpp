ec74c64cd770926175f99abe15f6bd9fa86b6964
shine.liu <shine.liu@rock-chips.com>
2022-04-27 11:15:20 +0800

    Audio: Add ext_x in devicefactory
    Signed-off-by: shine.liu <shine.liu@rock-chips.com>
    Change-Id: I65802c82f85133cd4a10a78149210bba6f7ca986
    (cherry picked from commit 30122b8dbd76872a966b07dffa0f86322c31fd1d)

--- audio/core/all-versions/default/DevicesFactory.cpp
+++ audio/core/all-versions/default/DevicesFactory.cpp
@@ -46,6 +46,17 @@ Return<void> DevicesFactory::openDevice(IDevicesFactory::Device device, openDevi
         case IDevicesFactory::Device::STUB:
             return openDevice(AUDIO_HARDWARE_MODULE_ID_STUB, _hidl_cb);
     }
+#if SUPPORT_MULTIAUDIO
+    if ((int32_t)device == 5) {
+        return openDevice(AUDIO_HARDWARE_MODULE_ID_EXT_1, _hidl_cb);
+    } else if ((int32_t)device == 6) {
+        return openDevice(AUDIO_HARDWARE_MODULE_ID_EXT_2, _hidl_cb);
+    } else if ((int32_t)device == 7) {
+        return openDevice(AUDIO_HARDWARE_MODULE_ID_EXT_3, _hidl_cb);
+    } else if ((int32_t)device == 8) {
+        return openDevice(AUDIO_HARDWARE_MODULE_ID_EXT_4, _hidl_cb);
+    }
+#endif
     _hidl_cb(Result::INVALID_ARGUMENTS, nullptr);
     return Void();
 }
